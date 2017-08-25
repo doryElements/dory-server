c647d03b8746f656b675a1c283338e2c2a9400da
BOUDVILLE Clement <boudville.clement@groupagrica.com>
2017-08-25 16:44:27 +0200

    improve profiles return

--- src/app/routes/profile.js
+++ src/app/routes/profile.js
@@ -10,7 +10,7 @@ const router = new Router({
 router.get('/',  (ctx,next) => {
     // console.log('req keys', Object.keys(req));
     // console.log('req authInfo',req.authInfo);
-    ctx.body ={message: 'ok', user: ctx.state.user, jwt: ctx.state.authInfo};
+    ctx.body ={message: 'ok', user: ctx.state.user.context.user, jwt: ctx.state.authInfo};
 });
 
 
